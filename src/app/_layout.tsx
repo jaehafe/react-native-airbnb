c0aa9437cfea565f497f1dc38be8ca7b305eee8c
Jaeha Lee <wogkdkrm112@gmail.com>
2024-03-17 15:10:48 +0900

    feat: login - expo-clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,33 @@
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
+
 import { useFonts } from 'expo-font';
 import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { X } from 'lucide-react-native';
-import { useEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
-
 export { ErrorBoundary } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+
+import { X } from 'lucide-react-native';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export const unstable_settings = {
   initialRouteName: '(tabs)',
@@ -20,11 +42,11 @@ export default function RootLayout() {
     'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
   });
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (error) throw error;
   }, [error]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
@@ -34,11 +56,22 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+
+  React.useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  }, [isLoaded]);
 
   return (
     <Stack>
